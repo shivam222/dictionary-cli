df13ba3a12d0c8cd437266b2f88f18aafecfb50d
shivam222 <shivamb61@gmail.com>
2019-12-22 02:55:00 +0530

    formatted

--- data/url.js
+++ data/url.js
@@ -3,11 +3,11 @@ const apihost = 'https://fourtytwowords.herokuapp.com';
 const api_key = 'b972c7ca44dda72a5b482052b1f5e13470e01477f3fb97c85d5313b3c112627073481104fec2fb1a0cc9d84c2212474c0cbe7d8e59d7b95c7cb32a1133f778abd1857bf934ba06647fda4f59e878d164';
 
 const url_def = word => {
-   return `${apihost}/word/${word}/definitions?api_key=${api_key}`;
+    return `${apihost}/word/${word}/definitions?api_key=${api_key}`;
 }
 
 const url_syn = word => {
-    return `${apihost}/word/${word}/relatedWords?api_key=${api_key}`;  
+    return `${apihost}/word/${word}/relatedWords?api_key=${api_key}`;
 }
 
 const url_ex = word => {
@@ -15,12 +15,12 @@ const url_ex = word => {
 }
 
 const url_rand = () => {
-    return `${apihost}/words/randomWord?api_key=${api_key}`;  
+    return `${apihost}/words/randomWord?api_key=${api_key}`;
 }
 
 module.exports = {
-    url_def:url_def,
-    url_syn:url_syn,
-    url_ex:url_ex,
-    url_rand:url_rand
+    url_def: url_def,
+    url_syn: url_syn,
+    url_ex: url_ex,
+    url_rand: url_rand
 }
--- dict.js
+++ dict.js
@@ -4,7 +4,7 @@
 const meow = require('meow');
 const processor = require('./lib/processor');
 const logger = require('./lib/logging');
-let action="";
+let action = "";
 
 const cli = meow(`
         Usage
@@ -15,96 +15,96 @@ const cli = meow(`
          $ dict start
 `);
 
-if(cli.input[0]) {
+if (cli.input[0]) {
     action = cli.input[0].toLowerCase();
 }
 
 switch (action) {
-    case 'defn': 
-         processor.getDefinition(cli.input[1])
-         .then( (defn) => {
-              logger.logInfo(`Definitions of the word "${defn.word}":`);
-              logger.logKeyInArray(defn.def, "text");
-         })
-         .catch( (err) => {
-              logger.logErr(err);
-         });
-     break;
+    case 'defn':
+        processor.getDefinition(cli.input[1])
+            .then((defn) => {
+                logger.logInfo(`Definitions of the word "${defn.word}":`);
+                logger.logKeyInArray(defn.def, "text");
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     case 'syn':
-         processor.getSynonyms(cli.input[1])
-         .then( (syno) => {
-            logger.logInfo(`Synonyms of the word "${syno.word}":`);
-            logger.logKeyInArray(syno.syn);
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });
-     break;
+        processor.getSynonyms(cli.input[1])
+            .then((syno) => {
+                logger.logInfo(`Synonyms of the word "${syno.word}":`);
+                logger.logKeyInArray(syno.syn);
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     case 'ant':
-         processor.getAntonyms(cli.input[1])
-         .then( (anto) => {
-            logger.logInfo(`Antonyms of the word "${anto.word}":`);
-            logger.logKeyInArray(anto.ant);
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });   
-     break;
+        processor.getAntonyms(cli.input[1])
+            .then((anto) => {
+                logger.logInfo(`Antonyms of the word "${anto.word}":`);
+                logger.logKeyInArray(anto.ant);
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     case 'ex':
-         processor.getExamples(cli.input[1])
-         .then( (exam) => {
-            logger.logInfo(`Examples of the word "${exam.word}":`);
-            logger.logKeyInArray(exam.examples, "text");
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });      
-     break;
+        processor.getExamples(cli.input[1])
+            .then((exam) => {
+                logger.logInfo(`Examples of the word "${exam.word}":`);
+                logger.logKeyInArray(exam.examples, "text");
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     case '':
-         processor.wordOfTheDay()
-         .then( (wod) => {
-            logger.logInfo(`Word of the day is "${wod.word}":`);
-            logger.logInfo(`Definitions of the word "${wod.word}":`);
-            logger.logKeyInArray(wod.def, "text");
-            logger.logInfo(`Synonyms of the word "${wod.word}":`);
-            logger.logKeyInArray(wod.syn);
-            logger.logInfo(`Antonyms of the word "${wod.word}":`);
-            logger.logKeyInArray(wod.ant);
-            logger.logInfo(`Examples of the word "${wod.word}":`);
-            logger.logKeyInArray(wod.ex, "text");    
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });
-     break;
+        processor.wordOfTheDay()
+            .then((wod) => {
+                logger.logInfo(`Word of the day is "${wod.word}":`);
+                logger.logInfo(`Definitions of the word "${wod.word}":`);
+                logger.logKeyInArray(wod.def, "text");
+                logger.logInfo(`Synonyms of the word "${wod.word}":`);
+                logger.logKeyInArray(wod.syn);
+                logger.logInfo(`Antonyms of the word "${wod.word}":`);
+                logger.logKeyInArray(wod.ant);
+                logger.logInfo(`Examples of the word "${wod.word}":`);
+                logger.logKeyInArray(wod.ex, "text");
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     case 'play':
-         processor.letsPlay()
-         .then( (result) => {
-            if(result) {
-                logger.logInfo("Good job:)");
-            } else {
-                logger.logInfo("Bye:|Keep learning");
-            }
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });
-     break;
+        processor.letsPlay()
+            .then((result) => {
+                if (result) {
+                    logger.logInfo("Good job:)");
+                } else {
+                    logger.logInfo("Bye:|Keep learning");
+                }
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
     default:
-         processor.getDetails(cli.input[0])
-         .then( (details) => {
-            logger.logInfo(`Definitions of the word "${details.word}":`);
-            logger.logKeyInArray(details.def, "text");
-            logger.logInfo(`Synonyms of the word "${details.word}":`);
-            logger.logKeyInArray(details.syn);
-            logger.logInfo(`Antonyms of the word "${details.word}":`);
-            logger.logKeyInArray(details.ant);
-            logger.logInfo(`Examples of the word "${details.word}":`);
-            logger.logKeyInArray(details.ex, "text");
-       })
-       .catch( (err) => {
-            logger.logErr(err);
-       });
-     break;
+        processor.getDetails(cli.input[0])
+            .then((details) => {
+                logger.logInfo(`Definitions of the word "${details.word}":`);
+                logger.logKeyInArray(details.def, "text");
+                logger.logInfo(`Synonyms of the word "${details.word}":`);
+                logger.logKeyInArray(details.syn);
+                logger.logInfo(`Antonyms of the word "${details.word}":`);
+                logger.logKeyInArray(details.ant);
+                logger.logInfo(`Examples of the word "${details.word}":`);
+                logger.logKeyInArray(details.ex, "text");
+            })
+            .catch((err) => {
+                logger.logErr(err);
+            });
+        break;
 }
 
