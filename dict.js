3b411ec48856300fb9342d6cd73dc465c7a1878a
shivam222 <shivamb61@gmail.com>
2019-12-21 17:42:26 +0530

    update

--- dict.js
+++ dict.js
@@ -20,25 +20,61 @@ if(cli.input[0]) {
 
 switch (action) {
     case 'defn': 
-         processor.getDefinition(cli.input[1]);
+         processor.getDefinition(cli.input[1])
+         .then( (defn) => {
+              console.log(defn); // better logging
+         })
+         .catch( (err) => {
+              console.log(err);
+         });
      break;
     case 'syn':
-         processor.getSynonyms(cli.input[1]);
+         processor.getSynonyms(cli.input[1])
+         .then( (syn) => {
+            console.log(syn); // better logging
+       })
+       .catch( (err) => {
+            console.log(err); // important formatting
+       });
      break;
     case 'ant':
-         processor.getAntonyms(cli.input[1]);    
+         processor.getAntonyms(cli.input[1])
+         .then( (ant) => {
+            console.log(ant); // better logging
+       })
+       .catch( (err) => {
+            console.log(err);
+       });   
      break;
     case 'ex':
-         processor.getExamples(cli.input[1]);      
+         processor.getExamples(cli.input[1])
+         .then( (ex) => {
+            console.log(ex); // better logging
+       })
+       .catch( (err) => {
+            console.log(err);
+       });      
      break;
     case '':
-         processor.wordOfTheDay();
+         processor.wordOfTheDay()
+         .then( (wod) => {
+            console.log(wod); // better logging
+       })
+       .catch( (err) => {
+            console.log(err);
+       });
      break;
     case 'play':
-        
+         processor.letsPlay();
      break;
     default:
-         processor.getDetails(cli.input[0]);
+         processor.getDetails(cli.input[0])
+         .then( (details) => {
+            console.log(details); // better logging
+       })
+       .catch( (err) => {
+            console.log(err);
+       });
      break;
 }
 
